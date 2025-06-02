29a3ba33942e15f1ea814793b42294c19cd272d0
Noface <166717111+Jerryslang@users.noreply.github.com>
2025-06-02 05:15:48 +0100

    Update client.py

--- client.py
+++ client.py
@@ -9,7 +9,15 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/js", StaticFiles(directory="static/js"), name="bundles")
 app.mount("/images", StaticFiles(directory="static/images"), name="images")
+app.mount("/flags", StaticFiles(directory="static/flags"), name="flags")
+app.mount("/fonts", StaticFiles(directory="static/fonts"), name="fonts")
+app.mount("/icons", StaticFiles(directory="static/icons"), name="icons")
+app.mount("/lang", StaticFiles(directory="static/lang"), name="languages")
+app.mount("/maps", StaticFiles(directory="static/maps"), name="maps")
+app.mount("/sprites", StaticFiles(directory="static/sprites"), name="sprites")
+
 app.mount("/mods", StaticFiles(directory="mods"), name="mods")
+
 
 @app.get("/")
 async def root():
